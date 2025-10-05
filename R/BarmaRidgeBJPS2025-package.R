3199a7c0cd90e21b718e2a75c621108924e936b2
everton-da-costa <everto.cost@gmail.com>
2025-10-05 01:31:07 -0300

    feat: Add R source code

---
+++ R/BarmaRidgeBJPS2025-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom dplyr %>%
+#' @importFrom ggplot2 ggplot
+#' @importFrom gridExtra grid.arrange
+#' @importFrom Rdpack reprompt
+#' @importFrom scales percent
+#' @importFrom zoo as.yearmon
+#' @importFrom doMC registerDoMC
+#' @importFrom foreach %dopar% foreach
+#' @importFrom doRNG %dorng%
+
+## usethis namespace: start
+#' @importFrom stats dbeta fitted frequency is.ts lm.fit make.link pchisq pnorm rbeta residuals start ts var
+## usethis namespace: end
+NULL
